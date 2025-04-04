40af7cd707e372b5888ede4b909b860dbbb491b2
Jhostin Aleck Sánchez <111006576+JhostinAleck@users.noreply.github.com>
2025-04-04 18:50:28 -0500

    as

--- dataManager/dataManager/urls.py
+++ dataManager/dataManager/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from receiver.views import receive_file
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('upload/', receive_file),
+
 ]
--- dataManager/reciever/models.py
+++ dataManager/reciever/models.py
@@ -1,3 +1,30 @@
-from django.db import models
+from django.http import JsonResponse
+import requests
+import os
 
-# Create your models here.
+STORAGE_URL = "http://<IP_VM_STORAGE>:8000/store/"
+
+def receive_file(request):
+    if request.method == 'POST' and request.FILES.get('file'):
+        file = request.FILES['file']
+        filename = file.name
+
+        with open(f"/tmp/{filename}", 'wb+') as dest:
+            for chunk in file.chunks():
+                dest.write(chunk)
+
+        data = {
+            "filename": filename,
+            "size": os.path.getsize(f"/tmp/{filename}")
+            # "metadata": metadata, 
+        }
+        # Simulación de envío a almacenamiento
+        with open(f"/tmp/{filename}", 'rb') as f:
+            response = requests.post(STORAGE_URL, data=data)
+
+        return JsonResponse({
+            "received": filename,
+            "sent_to_storage": response.status_code,
+        })
+
+    return JsonResponse({"error": "No file sent"}, status=400)
