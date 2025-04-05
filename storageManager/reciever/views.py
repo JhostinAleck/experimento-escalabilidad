62a3ef2664e54665f430c191b02dfe9a694d9fae
Jhostin Aleck Sánchez <111006576+JhostinAleck@users.noreply.github.com>
2025-04-04 21:02:33 -0500

    Update views.py

--- storageManager/reciever/views.py
+++ storageManager/reciever/views.py
@@ -5,16 +5,16 @@ from django.views.decorators.csrf import csrf_exempt
 def store_file(request):
     if request.method == 'POST':
         filename = request.POST.get('filename')
-        size = request.POST.get('size')
+        # size = request.POST.get('size')
 
-        if not filename or not size:
-            return JsonResponse({"error": "Missing filename or size"}, status=400)
+        if not filename :
+            return JsonResponse({"error": "Missing filename"}, status=400)
 
         # Aquí podrías guardar esta info en la base de datos o loguearla
         return JsonResponse({
             "status": "stored",
             "filename": filename,
-            "size": size
+            # "size": size
         })
 
     return JsonResponse({"error": "Invalid method"}, status=405)
