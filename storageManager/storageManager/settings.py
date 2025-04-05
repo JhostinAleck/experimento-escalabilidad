ba9e8c409e5774498d4ef73d1e147e114115494f
Jhostin Aleck Sánchez <111006576+JhostinAleck@users.noreply.github.com>
2025-04-04 20:17:21 -0500

    Update settings.py

--- storageManager/storageManager/settings.py
+++ storageManager/storageManager/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-9##bge5ry8h-b#%j@zu4vqi%mt)a=*vnlz=lfhy9h6&=+g3@6^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
