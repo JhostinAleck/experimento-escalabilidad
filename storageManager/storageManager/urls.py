6a71cb53ecdd7cfc9e64adb65e01f19d0791f63f
Jhostin Aleck Sánchez <111006576+JhostinAleck@users.noreply.github.com>
2025-04-04 19:17:27 -0500

    Update urls.py

--- storageManager/storageManager/urls.py
+++ storageManager/storageManager/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from receiver.views import store_file
+from reciever.views import store_file
 
 urlpatterns = [
     path('admin/', admin.site.urls),
